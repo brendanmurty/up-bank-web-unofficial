40f72199cd79fb361eb69830c53e8b6fadb88ddf
Brendan Murty <brendan@murty.au>
2024-11-26 15:07:36 +1100

    Update Next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: '/public',
-    runtimeCaching,
-  }
+  disable: process.env.NODE_ENV === 'development',
+  dest: '/public',
+  runtimeCaching,
 })
